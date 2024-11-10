6005c628d9ad902d997fe47b730f8fe80d533642
Ibrahim <ibrahim.srihi@student.vinci.be>
2024-11-10 10:24:55 +0100

    add : Test getSingleHourRow

--- tests/berlinClockTest.js
+++ tests/berlinClockTest.js
@@ -1,4 +1,4 @@
-const { gestSingleMinutesRow, get5MinutesRow } = require('../src/berlinClock');
+const { gestSingleMinutesRow, get5MinutesRow, getSingleHourRow } = require('../src/berlinClock');
 
 describe("Main should returns hours, minutes and seconds of the actual time",() => {
 
@@ -16,5 +16,15 @@ describe("Main should returns hours, minutes and seconds of the actual time",() 
         expect(get5MinutesRow(15)).toBe('YYROOOOOOOO');
         expect(get5MinutesRow(30)).toBe('YYRYYROOOOO');
     })
+
+    it("should return correct lamps for the single hours row", () => {
+        expect(getSingleHourRow(0)).toBe('OOOO');
+        expect(getSingleHourRow(1)).toBe('ROOO');
+        expect(getSingleHourRow(2)).toBe('RROO');
+        expect(getSingleHourRow(3)).toBe('RRRO');
+        expect(getSingleHourRow(4)).toBe('RRRR');
+    })
+
+    
 });
 
