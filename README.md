25a2e01fa32d6d0f8ba3aa0796c93444ccac9d7d
mathieuBleyenheuftVinci <165761155+mathieuBleyenheuftVinci@users.noreply.github.com>
2024-11-10 07:51:00 +0100

    add: test for minutes in berlinClockTest

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Participants :  - Bleyenheuft Mathieu 
                 - Pourbaix Mike
                 - Srihi Ibrahim 
+
+
+O = OFF
+Y = YELLOW
+R = RED
--- tests/berlinClockTest.js
+++ tests/berlinClockTest.js
@@ -1,12 +1,12 @@
-import { getBerlinClockTime } from '../src/berlinClock.js';
+const { gestSingleMinutesRow } = require('../src/berlinClock');
 
-describe("Main should returns hours, minutes and seconds of the actual time", function(){
-    let getBerlinClockTime = getBerlinClockTime();
+describe("Main should returns hours, minutes and seconds of the actual time",() => {
 
-    it("hours should return the actual hour", function(){
-        let hour = getBerlinClockTime.hours;
-        const date = new Date(timestamp);
-
-        expect(hour).toBe(date.getUTCHours());
+    it("should return correct lamps for the single minutes row", () => {
+        expect(gestSingleMinutesRow(0)).toBe('OOOO');
+        expect(gestSingleMinutesRow(1)).toBe('YOOO');
+        expect(gestSingleMinutesRow(2)).toBe('YYOO');
+        expect(gestSingleMinutesRow(3)).toBe('YYYO');
+        expect(gestSingleMinutesRow(4)).toBe('YYYY');
     });
 });
